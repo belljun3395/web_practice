b8a35a9aa21d2faf4fe0e6677dc330c94c8059e2
belljun3395 <195850@jnu.ac.kr>
2022-05-25 02:56:22 +0900

    web practice

--- src/middlewares/verify.js
+++ src/middlewares/verify.js
@@ -49,7 +49,6 @@ exports.join = async (req, res, next) => {
 
 exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
-    console.log(req.isAuthenticated())
     next();
   } else {
     res.redirect('/users/login');
