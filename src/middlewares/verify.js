d6408a65e27b478aac806956dfc8a6f1d3f9de8b
belljun3395 <195850@jnu.ac.kr>
2022-06-10 17:48:34 +0900

    test for swagger

--- src/middlewares/verify.js
+++ src/middlewares/verify.js
@@ -18,7 +18,8 @@ exports.authenticate = (req, res, next) => {
           console.error(loginError);
           return next(loginError);
         };
-        return res.redirect(`/${req.user.email}`);
+        return res.send("this is test")
+        // return res.redirect(`/${req.user.email}`);
       });
     })(req, res, next);
 };
